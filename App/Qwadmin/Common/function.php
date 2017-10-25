2a92a71fb23746d6e9a785256c660c45dce43d23
larker588 <larker588@163.com>
2017-10-25 15:46:05 +0800

    添加短信发送方法到公共库里

--- App/Qwadmin/Common/function.php
+++ App/Qwadmin/Common/function.php
@@ -4,40 +4,80 @@
  * @param $log
  * @param bool $name
  */
-
-function addlog($log, $name = false)
-{
-    $Model = M('log');
-    if (!$name) {
-        session_start();
-        $uid = session('uid');
-        if ($uid) {
-            $user = M('member')->field('user')->where(array('uid' => $uid))->find();
-            $data['name'] = $user['user'];
-        } else {
-            $data['name'] = '';
-        }
-    } else {
-        $data['name'] = $name;
-    }
-    $data['t'] = time();
-    $data['ip'] = $_SERVER["REMOTE_ADDR"];
-    $data['log'] = $log;
-    $Model->data($data)->add();
+function addlog($log, $name = false) {
+	$Model = M ( 'log' );
+	if (! $name) {
+		session_start ();
+		$uid = session ( 'uid' );
+		if ($uid) {
+			$user = M ( 'member' )->field ( 'user' )->where ( array (
+					'uid' => $uid 
+			) )->find ();
+			$data ['name'] = $user ['user'];
+		} else {
+			$data ['name'] = '';
+		}
+	} else {
+		$data ['name'] = $name;
+	}
+	$data ['t'] = time ();
+	$data ['ip'] = $_SERVER ["REMOTE_ADDR"];
+	$data ['log'] = $log;
+	$Model->data ( $data )->add ();
 }
 
+/**
+ * 获取用户信息
+ */
+function member($uid, $field = false) {
+	$model = M ( 'Member' );
+	if ($field) {
+		return $model->field ( $field )->where ( array (
+				'uid' => $uid 
+		) )->find ();
+	} else {
+		return $model->where ( array (
+				'uid' => $uid 
+		) )->find ();
+	}
+}
 
 /**
- *
- * 获取用户信息
- *
- **/
-function member($uid, $field = false)
-{
-    $model = M('Member');
-    if ($field) {
-        return $model->field($field)->where(array('uid' => $uid))->find();
-    } else {
-        return $model->where(array('uid' => $uid))->find();
-    }
+ * *
+ * 发送短信消息
+ * 
+ * @param $mobileArray 电话号码数组        	
+ * @param $content 短信消息        	
+ */
+function sendSmsMessage($mobileArray, $content) {
+	if(!empty($content)&&is_array($mobileArray)&&count($mobileArray)>0){//判断传值是否正确
+		$resultArray=array();//返回信息格式：
+		foreach ( $mobileArray as $mobile ) {
+			$post_data = array ();
+			$post_data ['sn'] = 'SDK_AAA_00227'; // 序列号
+			$post_data ['password'] = '852172777'; // 密码
+			$post_data ['content'] = $content;
+			$post_data ['mobile'] =$mobile; // 手机号
+			$post_data ['sendTime'] = date ( "YYYYMMDDHHmmss", time () ); // 定时发送，输入格式YYYYMMDDHHmmss的日期值
+			$post_data ['ext'] = ''; // 接入码
+			$url = 'http://123.56.233.239:8080/msg-core-web/msg/sendMsg';
+			$o = '';
+			foreach ( $post_data as $k => $v ) {
+				$o .= "$k=" . urlencode ( $v ) . '&';
+			}
+			$post_data = substr ( $o, 0, - 1 );
+			$ch = curl_init ();
+			curl_setopt ( $ch, CURLOPT_POST, 1 );
+			curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+			curl_setopt ( $ch, CURLOPT_URL, $url );
+			curl_setopt ( $ch, CURLOPT_POSTFIELDS, $post_data );
+			$result = curl_exec ( $ch );
+			array_push($resultArray, $result);
+		}
+		return $resultArray;
+	}else {
+		return null;
+	}
+	
+	
 }

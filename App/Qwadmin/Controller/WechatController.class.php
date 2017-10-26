f6bf715b0ee7eca3401f00b39b5a7bf6aa3c1f52
talver <talver@ubuntu>
2017-10-26 05:10:22 -0700

    1.登录界面增加微信授权登录链接； 2.新建微信授权登录控制器等；

---
+++ App/Qwadmin/Controller/WechatController.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * 版权：CopyRight (C) talver 2017
+ * 作者：talver (talver@126.com)
+ * 日期：Oct 26, 2017
+ * 版本：1.0.0
+ * 说明：微信登录授权
+ *
+ **/
+namespace Qwadmin\Controller;
+
+use Think\Controller;
+
+class WechatController extends Controller
+{
+
+    public function index()
+    {
+        $this->display();
+    }
+}

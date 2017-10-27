b4b37e890170761c2a314caf4a8e162295a322a6
larker588 <larker588@163.com>
2017-10-27 13:13:54 +0800

    短信模板完成

--- App/Qwadmin/Controller/SmsController.class.php
+++ App/Qwadmin/Controller/SmsController.class.php
@@ -12,63 +12,69 @@
 namespace Qwadmin\Controller;
 
 class SmsController extends ComController {
-	//短信模板管理
+	// 短信模板管理
 	public function smsindex() {
-		$data ['smstype'] = isset ( $_POST ['smstype'] ) ? trim ( $_POST ['smstype'] ) : 0; // 默认短信
-		$list = M ( 'em_smsmodel' )->where ( 'smstype=' . $data ['smstype'] )->select ();
-		$this->assign('smstype',$data['smstype']);
+		$data ['smstype'] = isset ( $_POST ['smstype'] ) ? trim ( $_POST ['smstype'] ) : 1; // 默认短信
+		$p = isset ( $_GET ['p'] ) ? intval ( $_GET ['p'] ) : '1';
+		$pagesize = 10; // 每页数量
+		$offset = $pagesize * ($p - 1); // 计算记录偏移量
+		$m = M ( 'em_smsmodel' );
+		$count = $m->where ( 'smstype=' . $data ['smstype'] )->count ();
+		$list = $m->where ( 'smstype=' . $data ['smstype'] )->limit ( $offset . ',' . $pagesize )->select ();
+		$page = new \Think\Page ( $count, $pagesize );
+		$page = $page->show ();
 		$this->assign ( 'list', $list );
+		$this->assign ( 'smstype', $data ['smstype'] );
+		$this->assign('page', $page);
 		$this->display ();
 	}
-	//编辑短信
-	public function edit(){
-		$id = isset($_GET['id']) ? intval($_GET['id']) : false;
-		if (!$id) {
-			$this->error('参数错误！');
+	// 编辑短信
+	public function edit() {
+		$id = isset ( $_GET ['id'] ) ? intval ( $_GET ['id'] ) : false;
+		if (! $id) {
+			$this->error ( '参数错误！' );
 		}
-		
-		$model=M('em_smsmodel')->where('id='.$id)->find();
-		$this->assign('model', $model);
-		$this->display('form');
+		$model = M ( 'em_smsmodel' )->where ( 'id=' . $id )->find ();
+		$this->assign ( 'model', $model );
+		$this->display ( 'form' );
 	}
 	
-	//添加或更新
-	public function update()
-	{
-		$model=D('em_smsmodel');
-		if(!empty($_POST)){
-			$model->create();//收集表单数据
+	// 添加或更新
+	public function update() {
+		$model = D ( 'em_smsmodel' );
+		if (! empty ( $_POST )) {
+			$model->create (); // 收集表单数据
 		}
-		dump(I('post.status', '', 'intval'));
-		/* 
-		if (isset ( $model->id )) { // 更新
-			$model->modifytime =date('y-m-d H:i:s',time());
+		if (! empty ( $model->id )) { // 更新
+			$model->modifytime = date ( 'y-m-d H:i:s', time () );
 			$model->modifier = session ( 'uid' );
-			$flag=$model->save ();
-			if($flag){
-				$this->success("保存成功");
-			}else{
-				$this->success("保存失败");
+			$model->status=I('post.status', '', 'intval');
+			$flag = $model->save ();
+			if ($flag) {
+				$this->success ( "保存成功" );
+			} else {
+				$this->success ( "保存失败" );
 			}
 		} else { // 新增
-			$model->createtime = date('y-m-d H:i:s',time());
+			$model->createtime = date ( 'y-m-d H:i:s', time () );
 			$model->creater = session ( 'uid' );
-			$flag=$model->add ();
-			if($flag){
-				$this->success("创建成功");
-			}else{
-				$this->success("创建失败");
+			$model->status = 1;
+			$flag = $model->add ();
+			if ($flag) {
+				$this->success ( "创建成功" );
+			} else {
+				$this->success ( "创建失败" );
 			}
-		} */
+		}
 	}
-	
-	
-	public function add()
-	{
-		
-		$option = M('auth_rule')->order('o ASC')->select();
-		$option = $this->getMenu($option);
-		$this->assign('option', $option);
-		$this->display('form');
+	/**
+	 * 添加短信模板
+	 */
+	public function add() {
+		$model = M ( 'em_smsmodel' )->create ();
+		$model ['status'] = 1;
+		$model ['smstype'] = 1;
+		$this->assign ( 'model', $model );
+		$this->display ( 'form' );
 	}
 }

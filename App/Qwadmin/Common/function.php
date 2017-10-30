e805ac783e3ee85d01a9d71a97b1c8de9f5b9cee
larker588 <larker588@163.com>
2017-10-30 12:51:00 +0800

    添加uuid函数

--- App/Qwadmin/Common/function.php
+++ App/Qwadmin/Common/function.php
@@ -85,7 +85,7 @@ function sendSmsMessage($mobileArray, $content) {
  * *
  * 生成随机数
  *
- * @param $length 随机数商都        	
+ * @param $length 生成随机数	
  * @return 随机数
  */
 function getRandNumber($length) {
@@ -94,4 +94,18 @@ function getRandNumber($length) {
 		$b [] = array_rand ( $a );
 	}
 	return join ( "", $b );
+}
+
+
+
+/***
+ * 创建uuid
+ * @return string
+ */
+function guid() {
+	mt_srand ( ( double ) microtime () * 10000 );
+	$charid = strtoupper ( md5 ( uniqid ( rand (), true ) ) );
+	$hyphen = chr ( 45 ); // "-"
+	$uuid = substr ( $charid, 0, 8 ) . $hyphen . substr ( $charid, 8, 4 ) . $hyphen . substr ( $charid, 12, 4 ) . $hyphen . substr ( $charid, 16, 4 ) . $hyphen . substr ( $charid, 20, 12 );
+	return $uuid;
 }

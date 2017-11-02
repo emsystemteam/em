55cc4fd3ba1def69b73ef3ad7bfe3b426d1b2c91
talver <talver@ubuntu>
2017-11-02 04:33:15 -0700

    1.微信授权

--- App/Qwadmin/Controller/WechatController.class.php
+++ App/Qwadmin/Controller/WechatController.class.php
@@ -25,9 +25,9 @@ class WechatController extends Controller
     public function index()
     {
         $this->redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . U('wechat/authorize');
-        $state = "abb7e6700ecb5fa8a384";
-        // redirect($this->get_authorize_url($state));
-        redirect($this->redirect_uri . "?code=1231321&state={$state}");
+        $state = md5(time() + getRandNumber(10));
+        redirect($this->get_authorize_url($state));
+        // redirect($this->redirect_uri . "?code=1231321&state={$state}");
     }
 
     public function authorize()
@@ -52,7 +52,7 @@ class WechatController extends Controller
                 $this->display("authorize");
             }
         } else {
-            $this->error("授权失败！", U('login/index'));
+            $this->error("获取微信登录授权失败！", U('login/index'));
         }
     }
 
@@ -201,11 +201,7 @@ class WechatController extends Controller
             $post_data['sendTime'] = date("YYYYMMDDHHmmss", time()); // 定时发送，输入格式YYYYMMDDHHmmss的日期值
             $post_data['ext'] = ''; // 接入码
             $url = 'http://123.56.233.239:8080/msg-core-web/msg/sendMsg';
-            $o = '';
-            foreach ($post_data as $k => $v) {
-                $o .= "$k=" . urlencode($v) . '&';
-            }
-            $post_data = substr($o, 0, - 1);
+            $post_data = http_build_query($post_data);
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -223,7 +219,12 @@ class WechatController extends Controller
 
     function get_openid($code)
     {
-        return '3243242343243242';
+        if (strlen($code) == 0)
+            return "";
+        $token = $this->get_access_token($code);
+        if (strlen($token) == 0)
+            return "";
+        return $token;
     }
 
     /**
@@ -237,7 +238,7 @@ class WechatController extends Controller
     function get_authorize_url($state)
     {
         $redirect_uri = urlencode($this->redirect_uri);
-        return "https: // open.weixin.qq.com/connect/qrconnect?appid={$this->app_id}&redirect_uri={$redirect_uri}&response_type=code&scope=snsapi_login&state={$state}#wechat_redirect";
+        return "https://open.weixin.qq.com/connect/qrconnect?appid={$this->app_id}&redirect_uri={$redirect_uri}&response_type=code&scope=snsapi_login&state={$state}#wechat_redirect";
     }
 
     /**
@@ -248,17 +249,18 @@ class WechatController extends Controller
      */
     function get_access_token($code)
     {
+        if (strlen($code) == 0)
+            return "";
         $token_url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={$this->app_id}&secret={$this->app_secret}&code={$code}&grant_type=authorization_code";
         $token_data = $this->http($token_url);
-        
-        if ($token_data[0] == 200) {
-            return json_decode($token_data[1], TRUE);
+        $json = json_decode($token_data);
+        if (isset($json->openid)) {
+            return $json->openid;
         }
-        
-        return FALSE;
+        return "";
     }
 
-    function http($url, $method, $postfields = null, $headers = array(), $debug = false)
+    function http($url)
     {
         $ci = curl_init();
         /* Curl settings */
@@ -268,37 +270,11 @@ class WechatController extends Controller
         curl_setopt($ci, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ci, CURLOPT_TIMEOUT, 30);
         curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
-        
-        switch ($method) {
-            case 'POST':
-                curl_setopt($ci, CURLOPT_POST, true);
-                if (! empty($postfields)) {
-                    curl_setopt($ci, CURLOPT_POSTFIELDS, $postfields);
-                    $this->postdata = $postfields;
-                }
-                break;
-        }
+        curl_setopt($ci, CURLOPT_POST, true);
         curl_setopt($ci, CURLOPT_URL, $url);
-        curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ci, CURLINFO_HEADER_OUT, true);
-        
         $response = curl_exec($ci);
-        $http_code = curl_getinfo($ci, CURLINFO_HTTP_CODE);
-        
-        if ($debug) {
-            echo "=====post data======\r\n";
-            var_dump($postfields);
-            
-            echo '=====info=====' . "\r\n";
-            print_r(curl_getinfo($ci));
-            
-            echo '=====$response=====' . "\r\n";
-            print_r($response);
-        }
         curl_close($ci);
-        return array(
-            $http_code,
-            $response
-        );
+        return $response;
     }
 }

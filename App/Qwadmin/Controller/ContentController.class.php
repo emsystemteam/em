c0d663f4bc89f02ccb0b2af364c12876a2ceab20
larker588 <larker588@163.com>
2017-11-06 17:49:21 +0800

    模块管理暂存

---
+++ App/Qwadmin/Controller/ContentController.class.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ *
+ * 版权所有：乐助科技
+ * 作    者：liangc
+ * 日    期：2017-11-6
+ * 版    本：1.0.0
+ * 功能说明：公告通知
+ *
+ **/
+namespace Qwadmin\Controller;
+
+class ContentController extends ComController {
+	// 内容管理首页
+	public function index() {
+		$p = isset ( $_GET ['p'] ) ? intval ( $_GET ['p'] ) : '1';
+		$pagesize = 10; // 每页数量
+		$offset = $pagesize * ($p - 1); // 计算记录偏移量
+		$m = M ( 'em_contentmanager' );
+		$count = $m->where ( 'status=1' )->count ();
+		$list = $m->where ( 'status=1' )->order ( 'createtime desc' )->limit ( $offset . ',' . $pagesize )->select ();
+		$page = new \Think\Page ( $count, $pagesize );
+		$page = $page->show ();
+		$this->assign ( 'list', $list );
+		$this->assign ( 'page', $page );
+		$this->display ();
+	}
+	
+	// 模块管理明细-文章列表
+	public function detail() {
+		$m = M ( 'em_contentmanager' );
+		$count = $m->where ( 'status=1' )->count ();
+		if (isset ( $_GET ['id'] )) {
+			$id = intval ( $_GET ['id'] );
+			$p = isset ( $_GET ['p'] ) ? intval ( $_GET ['p'] ) : '1';
+			$pagesize = 10; // 每页数量
+			$offset = $pagesize * ($p - 1); // 计算记录偏移量
+			$m=M('em_notice');
+			$count = $m->where ( 'stauts=1 and id='.$id )->count ();
+			$list = $m->where ( 'stauts=1 and id='.$id )->order ( 'createtime desc' )->limit ( $offset . ',' . $pagesize )->select ();
+			if(!$list){
+				$this->error('没有找到任何文章信息');
+			}
+			$page = new \Think\Page ( $count, $pagesize );
+			$page = $page->show ();
+			$this->assign ( 'list', $list );
+		}else{
+			$this->error('没有找到任何文章信息');
+		}
+	}
+}

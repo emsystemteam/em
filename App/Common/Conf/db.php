b8963de42ca74bd8aaf50c99cb5b05febbcb4c3d
xuluo7742 <xuluo7742@163.com>
2017-11-24 23:38:02 +0800

    1.解决冲突

--- App/Common/Conf/db.php
+++ App/Common/Conf/db.php
@@ -12,9 +12,12 @@ return array(
     'DB_TYPE' => 'mysql', // 数据库类型
     'DB_HOST' => 'localhost', // 服务器地址
     'DB_NAME' => 'qwadmin', // 数据库名
-    'DB_USER' => 'talver', // 用户名
-    'DB_PWD' => 'dev1235', // 密码
+    'DB_USER' => 'root', // 用户名
+    'DB_PWD' => '', // 密码
     'DB_PORT' => 3306, // 端口
     'DB_PREFIX' => 'qw_', // 数据库表前缀
     'DB_CHARSET' => 'utf8' // 数据库编码默认采用utf8
+	//'DB_PARAMS' => array (
+	//		\PDO::ATTR_CASE => \PDO::CASE_NATURAL
+	//)  // 数据库不区分大小写
 );

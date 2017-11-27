26e7394b18c122164cc7cbedebcc9ba2eea3e241
xuluo7742 <xuluo7742@163.com>
2017-11-27 23:07:02 +0800

    1.大写字段改小写

--- qw_em_vote.sql
+++ qw_em_vote.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50714
 File Encoding         : 65001
 
-Date: 2017-11-26 23:05:03
+Date: 2017-11-27 23:06:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -33,9 +33,9 @@ CREATE TABLE `qw_em_vote` (
   `wechat_push_times` int(11) DEFAULT NULL,
   `vote_status` int(11) NOT NULL COMMENT '1.未录入，2未开始，3已发布，4.已结束，关联字典表',
   `order_filed` int(11) DEFAULT NULL COMMENT 'int型，从1开始，越小优先级越高',
-  `CREATE_TIME` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  `MODIFY_TIME` timestamp NULL DEFAULT NULL COMMENT '修改时间',
-  `OPERATOR` int(11) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `modify_time` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  `operator` int(11) DEFAULT NULL,
   PRIMARY KEY (`vote_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='投票主表,保存投票基本信息';
 
@@ -50,9 +50,9 @@ CREATE TABLE `qw_em_vote_paper` (
   `question_titile` varchar(255) NOT NULL,
   `question_desc` varchar(255) DEFAULT NULL,
   `order_filed` int(11) DEFAULT NULL COMMENT 'int型，从1开始，越小优先级越高',
-  `CREATE_TIME` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  `MODIFY_TIME` timestamp NULL DEFAULT NULL COMMENT '修改时间',
-  `OPERATOR` int(11) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `modify_time` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  `operator` int(11) DEFAULT NULL,
   PRIMARY KEY (`vote_paper_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='投票问卷表';
 
@@ -67,9 +67,9 @@ CREATE TABLE `qw_em_vote_paper_option` (
   `option_desc` varchar(255) DEFAULT NULL,
   `option_pic` varchar(255) DEFAULT NULL,
   `order_filed` int(11) DEFAULT NULL COMMENT 'int型，从1开始，越小优先级越高',
-  `CREATE_TIME` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  `MODIFY_TIME` timestamp NULL DEFAULT NULL COMMENT '修改时间',
-  `OPERATOR` int(11) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `modify_time` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  `operator` int(11) DEFAULT NULL,
   PRIMARY KEY (`vote_paper_option_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='投票问卷选项表';
 
@@ -86,9 +86,9 @@ CREATE TABLE `qw_em_vote_result` (
   `vote_type` int(11) NOT NULL COMMENT '1.业主投票，2.补录，关联字典表',
   `user_id` int(11) NOT NULL COMMENT '投票用户，关联用户表',
   `order_filed` int(11) DEFAULT NULL COMMENT 'int型，从1开始，越小优先级越高',
-  `CREATE_TIME` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  `MODIFY_TIME` timestamp NULL DEFAULT NULL COMMENT '修改时间',
-  `OPERATOR` int(11) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `modify_time` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  `operator` int(11) DEFAULT NULL,
   PRIMARY KEY (`vote_result_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='投票结果表';
 
